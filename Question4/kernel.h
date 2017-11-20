d9a6c7a935929562d536a088fc75230e98252942
Adam <pipited02@gmail.com>
2017-11-20 20:22:34 +0800

    整理一下

---
+++ Question4/kernel.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#define N 14 //14­ÓQ­È
+
+void Allocate_Memory();
+
+void Free_Memory();
+
+void Send_To_Device();
+
+void Get_From_Memory();
+
+void Launch_hardy_cross();
